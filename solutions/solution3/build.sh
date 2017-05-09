1052b21a301934c3e1b0da0168fd358b171d7172
Mateusz Małek <kontakt@mateuszmalek.pl>
2017-05-09 20:03:51 +0200

    solution3 fixes

--- solutions/solution3/build.sh
+++ solutions/solution3/build.sh
@@ -3,6 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/bin
 rm -f *
-qmake -makefile ../src/FaceFeatures.pro
+qmake -makefile ../src/solution3.pro
 make
-mv FaceFeatures solution3
--- solutions/solution3/src/solution3.pro
+++ solutions/solution3/src/solution3.pro
@@ -3,7 +3,7 @@ QT += gui
 
 CONFIG += c++11
 
-TARGET = FaceFeatures
+TARGET = solution3
 CONFIG += console
 CONFIG -= app_bundle
 
