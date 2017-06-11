97e806a9509934cb56e9534710da59113271304e
Mateusz Małek <kontakt@mateuszmalek.pl>
2017-06-11 19:05:10 +0200

    solution3: removed obsolete file

--- solutions/solution3/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [ -f $DIR/deploy/solution3 ]; then
-    $DIR/deploy/solution3 1 /home/tajgeer/Repozytoria/ObliczeniaInteligentne/environment/data/test/images /home/tajgeer/Repozytoria/NeuralNetwork/results
-fi
